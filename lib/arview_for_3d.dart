b35349fd1bcbdcbab2b71b984b4bd1f91be39760
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-14 05:14:18 +0200

    Add the needed packages for the ar viewer

--- lib/arview_for_3d.dart
+++ lib/arview_for_3d.dart
@@ -1,4 +1,16 @@
+import 'package:ar_flutter_plugin_engine/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin_engine/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin_engine/datatypes/hittest_result_types.dart';
+import 'package:ar_flutter_plugin_engine/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin_engine/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin_engine/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin_engine/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin_engine/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin_engine/models/ar_anchor.dart';
+import 'package:ar_flutter_plugin_engine/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin_engine/models/ar_node.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vectorMath64;
 
 class ArViewFor3dObjects extends StatefulWidget {
   const ArViewFor3dObjects({super.key});
